f61fd271e578b83e214fd141eed99f64190cfb3f
Nguyễn Thành Luân <thanhluanqn1998@gmail.com>
2019-04-21 17:18:11 +0700

    Update teamLogos.js

--- src/utilities/teamLogos.js
+++ src/utilities/teamLogos.js
@@ -4,25 +4,25 @@
 
 const teamLogoUrls = {
     668:
-        "https://en.wikipedia.org/wiki/VVV-Venlo#/media/File:VVV-Venlo_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/VVV-Venlo_logo.svg/800px-VVV-Venlo_logo.svg.png",
     680:
-        "https://en.wikipedia.org/wiki/ADO_Den_Haag#/media/File:ADO_Den_Haag_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/ADO_Den_Haag_logo.svg/800px-ADO_Den_Haag_logo.svg.png",
     681:
         "https://en.wikipedia.org/wiki/NAC_Breda#/media/File:Logo_NAC_Breda.png",
     1913:
-        "https://en.wikipedia.org/wiki/De_Graafschap#/media/File:VBV_De_Graafschap_Doetinchem.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/VBV_De_Graafschap_Doetinchem.svg/800px-VBV_De_Graafschap_Doetinchem.svg.png",
     1914:
-        "https://en.wikipedia.org/wiki/FC_Emmen#/media/File:FC_Emmen_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/8/83/FC_Emmen_logo.svg/800px-FC_Emmen_logo.svg.png",
     1920:
-        "https://en.wikipedia.org/wiki/Fortuna_Sittard#/media/File:Fortuna_Sittard_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Fortuna_Sittard_logo.svg/800px-Fortuna_Sittard_logo.svg.png",
     77:
         "https://upload.wikimedia.org/wikipedia/vi/8/8c/Athletic_c_de_bilbao.gif",
     88:
-        "https://vi.wikipedia.org/wiki/Levante_UD#/media/File:Escut-Penya-Totil.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Escut-Penya-Totil.svg/800px-Escut-Penya-Totil.svg.png",
     559:
-        "https://vi.wikipedia.org/wiki/Sevilla_F.C.#/media/File:Sevilla_fc.gif",
+        "https://upload.wikimedia.org/wikipedia/vi/4/45/Sevilla_fc.gif",
     576:
-        "https://en.wikipedia.org/wiki/RC_Strasbourg_Alsace#/media/File:Racing_Club_de_Strasbourg_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Racing_Club_de_Strasbourg_logo.svg/800px-Racing_Club_de_Strasbourg_logo.svg.png",
     59:
         "https://vi.wikipedia.org/wiki/Blackburn_Rovers_F.C.#/media/File:Blackburn_crest.png",
     60:
@@ -30,7 +30,7 @@ const teamLogoUrls = {
     74:
         "https://vi.wikipedia.org/wiki/West_Bromwich_Albion_F.C.#/media/File:West_Bromwich_Albion.png",
     322:
-        "https://en.wikipedia.org/wiki/Hull_City_A.F.C.#/media/File:Hull_City_Crest_2014.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Hull_City_Crest_2014.svg/800px-Hull_City_Crest_2014.svg.png",
     341:
         "https://vi.wikipedia.org/wiki/Leeds_United_F.C.#/media/File:Leeds_United_Logo.png",
     355:
@@ -38,7 +38,7 @@ const teamLogoUrls = {
     387:
         "https://en.wikipedia.org/wiki/Bristol_City_F.C.#/media/File:Bristol_City_FC_logo.png",
     402:
-        "https://en.wikipedia.org/wiki/Brentford_F.C.#/media/File:Brentford_FC_crest.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/Brentford_FC_crest.svg/800px-Brentford_FC_crest.svg.png",
     1081:
         "https://upload.wikimedia.org/wikipedia/en/2/21/PNE_FC.png",
     496:
@@ -46,13 +46,13 @@ const teamLogoUrls = {
     498:
         "https://upload.wikimedia.org/wikipedia/en/e/e1/Sporting_Clube_de_Portugal_%28Logo%29.svg",
     1903:
-        "https://en.wikipedia.org/wiki/S.L._Benfica#/media/File:SL_Benfica_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/SL_Benfica_logo.svg/800px-SL_Benfica_logo.svg.png",
     5529:
-        "https://en.wikipedia.org/wiki/C.D._Nacional#/media/File:C.D._Nacional.gif",
+        "https://upload.wikimedia.org/wikipedia/en/9/96/C.D._Nacional.gif",
     5530:
-        "https://en.wikipedia.org/wiki/C.D._Santa_Clara#/media/File:C.D._Santa_Clara_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/C.D._Santa_Clara_logo.svg/800px-C.D._Santa_Clara_logo.svg.png",
     5543:
-        "https://en.wikipedia.org/wiki/Vit%C3%B3ria_S.C.#/media/File:Vit%C3%B3ria_Guimar%C3%A3es.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Vit%C3%B3ria_Guimar%C3%A3es.svg/800px-Vit%C3%B3ria_Guimar%C3%A3es.svg.png",
     5544:
         "https://en.wikipedia.org/wiki/C.D._Aves#/media/File:CD_Aves_logo.png",
     5565:
@@ -64,13 +64,13 @@ const teamLogoUrls = {
     5601:
         "https://en.wikipedia.org/wiki/Portimonense_S.C.#/media/File:Portimonense_Sporting_Clube.png",
     5613:
-        "https://en.wikipedia.org/wiki/S.C._Braga#/media/File:S.C._Braga_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/7/79/S.C._Braga_logo.svg/800px-S.C._Braga_logo.svg.png",
     5620:
         "https://en.wikipedia.org/wiki/Vit%C3%B3ria_F.C.#/media/File:Vit%C3%B3ria_F.C._logo.png",
     584:
         "https://upload.wikimedia.org/wikipedia/en/d/d2/U.C._Sampdoria_logo.svg",
     1107:
-        "https://en.wikipedia.org/wiki/S.P.A.L.#/media/File:Spal2013_logo.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Spal2013_logo.svg/800px-Spal2013_logo.svg.png",
 }
 
 export default teamLogoUrls;
